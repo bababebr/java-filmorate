3da74f7785332a65441344a85c7ff03aed10e302
bababebr <unior1234@yandex.ru>
2023-03-24 14:51:34 +0300

    refactror: исправления #1.1

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.errors.ErrorResponse;
 
+import javax.validation.ValidationException;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -12,25 +14,25 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleHappinessOverflow(final ValidationException e) {
-        return Map.of("Объект не прошел валидацию: ", e.getMessage());
+    public ErrorResponse handleHappinessOverflow(final ValidationException e) {
+        return new ErrorResponse("Объект не прошел валидацию: ", e.getMessage());
     }
 
     @ExceptionHandler({NoSuchUserException.class, FriendServiceException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> userControllerException(final RuntimeException e) {
-        return Map.of("Ошибка Пользователя: ", e.getMessage());
+    public ErrorResponse userControllerException(final RuntimeException e) {
+        return new ErrorResponse("Ошибка Пользователя: ", e.getMessage());
     }
 
     @ExceptionHandler({NoSuchFilmException.class, FilmServiceException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> filmControllerException(final RuntimeException e) {
-        return Map.of("Ошибка Фильмов: ", e.getMessage());
+    public ErrorResponse filmControllerException(final RuntimeException e) {
+        return new ErrorResponse("Ошибка Фильмов: ", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> serverException(final Exception e) {
-        return Map.of("Ошибка сервера: ", e.getMessage());
+    public ErrorResponse serverException(final Exception e) {
+        return new ErrorResponse("Ошибка сервера: ", e.getMessage());
     }
 }
